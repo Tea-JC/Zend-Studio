418ddd665da468a8b5128c3d1dcd27b997df5800
Tea-JC <1316032599@qq.com>
2018-08-06 18:38:10 +0800

    8.6

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+echo "hello world";
+echo "hello world"; // 复制当前行：ctrl+alt+↓
+                    
+// 交换上下行:alt+↑/↓
+echo "world";
+echo "hello";
+
+
+//删除当前行:ctrl+d
+
+
+//行注释：ctrl+/
+// echo "hello world";
+// echo "hello world";
+// echo "hello world";
+// echo "hello world";
+
+//块注释:ctrl+shift+/
+/* echo "hello world";
+echo "hello world";
+echo "hello world";
+echo "hello world"; */
+
+//提示助手:alt+/
+$min=1;
+$max=2;
+//跳转到行的最后:end
+rand(min,max);
+
+//面板搜索：ctrl+f
+
+//全文检索:ctrl+h
+
+//代码格式化:ctrl+shift+f
+
+//关闭当前文件:ctrl+w
+
+//所有快捷键列表:ctrl+shift+l
+//修改快捷键：window下的prefence里的general的keys
